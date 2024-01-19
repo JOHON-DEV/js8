0a8e5c01cc11a21c226ebd2a8df255b6c31d2927
JOHON-DEV <machoman4701@gmail.com>
2024-01-19 07:24:40 +0500

    js-8 lesson

---
+++ index.js
@@ -0,0 +1,133 @@
+// function yigindi(n) {
+//   let natija = 0;
+//   for (s = 1; s <= n; s++) {
+//     natija = + 1/ s;
+//   }
+// console.log(natija)
+// }
+
+// yigindi(18)
+
+// function teskari(n){
+//     for(i=n;i>0;i--){
+
+//     console.log(i)
+//     }
+
+// }
+// teskari(10)
+
+// function teskari(n) {
+//   let kopaytma = 1;
+//   for (i = n; i <= 1; i++) {
+//     kopaytma *= i;
+
+//     if(n%2==1){
+//         console.log(i)
+//     }
+//   }
+// }
+// teskari(10);
+
+// 1-misol
+// function daraja(n) {
+//   return Math.pow(n,3)
+
+// }
+// let n=4
+// console.log(daraja(n));
+
+// 2-misol
+// function yigindi(a,b){
+//     return a+b
+// }
+// let a =6
+// let b =7
+// console.log(yigindi(a,b))
+
+// 3-misol
+// function ayirma(a,b){
+//     return a-b
+// }
+// let a=10
+// let b=6
+// console.log(ayirma(a,b))
+
+// 4-misol
+// function daraja(n){
+//     for(i=2;i<=4;i++){
+//         console.log(Math.pow(n,i))
+//     }
+// }
+// let n = 5
+// daraja(n)
+
+// 5-misol
+// function toq(n){
+//     for(let i = 1; i <= n; i++){
+//         if(n%2 != 0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// let n = 23;
+// toq(n);
+
+// 6-misol
+// function juft(n){
+//     for(let i = 1; i <= n; i++){
+//         if(n%2 == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+// let n = 23;
+// juft(n);
+
+// 7-misol
+// function increment(n){
+//     console.log(n+1);
+//     console.log(n-1);
+// }
+
+// let n = 10;
+// increment(n);
+
+// 8-misol
+// function boluv(n){
+//     for(let i = 1; i<=n; i++){
+//         if(n%i==0)
+//             console.log(i);
+//     }
+// }
+
+// let n = 25;
+// boluv(n);
+
+// 9-misol
+// function calc(A, B, S){
+//     if(S == "*")
+//         return A * B;
+//     else if(S == "/")
+//         return A / B;
+//     else if(S == "-")
+//         return A - B;
+//     else if(S == "+")
+//         return A + B;
+//     else
+//         return 0;
+// }
+
+// let a = 10;
+// let b = 15;
+// let s = "*";
+// console.log(calc(a,b,s));
+
+// 10-misol
+// for (let index = 1; index <= 9; index++) {
+//    for (let i = 1; i <= 9; i++) {
+//         console.log(`${index} * ${i} = ${index*i}`);
+//    }
+    
+// }
